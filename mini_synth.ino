b3c7dab443ea40674ebb249ca8b12091d750fe07
Marcel Licence <marcel.licence@gmx.de>
2026-02-19 21:31:56 +0100

    Use SYNTH_PARAM_MODULATION_SPEED to control actCh instead of channel 0

--- mini_synth.ino
+++ mini_synth.ino
@@ -781,7 +781,7 @@ void Synth_SetParam(uint8_t param, float value)
         break;
 
     case SYNTH_PARAM_MODULATION_SPEED:
-        chControl[0].modulation.speed = value * 128;
+        chControl[actCh].modulation.speed = value * 128;
         break;
 
     case SYNTH_PARAM_MODULATION_PITCH:
